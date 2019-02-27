546a76961cf93d28fa2dd101e6cbe6558468dde2
bharanikarthi <47816055+bharanikarthi@users.noreply.github.com>
2019-02-27 16:07:06 +0530

    Create print.c

---
+++ print.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,h;
+printf("enter the value");
+scanf("%d",&n);
+{
+printf("the values are");
+else
+{
+printf("%d",&n);
+}
+getch();
+}
+}
